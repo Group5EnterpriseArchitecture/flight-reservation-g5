06c9ce666571349a8f0cdaf5c2d1bb3884bbf6e5
Eskoch <abebeeskender@gmial.com>
2021-08-08 16:16:55 -0500

    [teamB] AirlineDTO and FlightDTO

--- src/main/java/edu/miu/cs/cs544/flightreservation/DTO/FlightDTO.java
+++ src/main/java/edu/miu/cs/cs544/flightreservation/DTO/FlightDTO.java
@@ -11,12 +11,19 @@ public class FlightDTO {
     private Long id;
     private String flightNumber;
     private Integer capacity;
-
     private AirportDTO departure;
-
     private AirportDTO arrival;
     private LocalTime departureTime;
     private LocalTime arrivalTime;
     private AirlineDTO operateBy;
 
+    public FlightDTO(String flightNumber, Integer capacity, AirportDTO departure, AirportDTO arrival, LocalTime departureTime, LocalTime arrivalTime, AirlineDTO operateBy) {
+        this.flightNumber = flightNumber;
+        this.capacity = capacity;
+        this.departure = departure;
+        this.arrival = arrival;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.operateBy = operateBy;
+    }
 }
--- src/main/java/edu/miu/cs/cs544/flightreservation/domain/Airline.java
+++ src/main/java/edu/miu/cs/cs544/flightreservation/domain/Airline.java
@@ -1,12 +1,14 @@
 package edu.miu.cs.cs544.flightreservation.domain;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @SecondaryTable(name="airline_history")
 @Data
+@NoArgsConstructor
 public class Airline {
 
     @Id
@@ -18,4 +20,9 @@ public class Airline {
     @Column(table = "airline_history")
     private String history;
 
+    public Airline(String code, String name, String history) {
+        this.code = code;
+        this.name = name;
+        this.history = history;
+    }
 }
