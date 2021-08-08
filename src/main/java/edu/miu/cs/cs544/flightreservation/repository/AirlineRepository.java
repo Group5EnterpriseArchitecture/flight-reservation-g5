beae8323ff3209b3c8dc144daa4e00f46942e1f5
Alvin Abaho <abahoalvin@gmail.com>
2021-08-08 17:08:28 -0500

    [Henok] Airline repository is @Transactional

--- src/main/java/edu/miu/cs/cs544/flightreservation/repository/AirlineRepository.java
+++ src/main/java/edu/miu/cs/cs544/flightreservation/repository/AirlineRepository.java
@@ -3,10 +3,12 @@ package edu.miu.cs.cs544.flightreservation.repository;
 import edu.miu.cs.cs544.flightreservation.domain.Airline;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
+@Transactional
 public interface AirlineRepository extends JpaRepository<Airline, Long> {
     Optional<Airline> findAirlineByCode(String code);
 }
